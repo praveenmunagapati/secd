3a48f871bbf5748552e78d68d88d41e8d4965d1f
praveenmun <mpraveenpro@gmail.com>
2026-09-10 14:27:31 +0530

    Update logical operator examples in main.c for improved clarity and correctness

--- main.c
+++ main.c
@@ -2,18 +2,7 @@
 #define PI 3.14
 int main(void) {
     setbuf(stdout,0);
-    //logical operators
-    //and &&
-    // return 1 if both statements are true
-    // or ||
-    // logical not !
 
-    int a = 100;
-    int b = 15;
-    int c = 600;
-    printf("is a big %d\n",a>b&&a>c);
-    printf("%d\n",a>b||a>c);
-    printf("%d\n",!0);
 
 
 return 0;
