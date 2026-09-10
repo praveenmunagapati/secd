0a7e2774c0b7e7d0c7f74390ab0d4b2433862913
praveenmun <mpraveenpro@gmail.com>
2026-09-10 14:25:03 +0530

    Refactor main.c by updating logical operator examples and improving code clarity

--- main.c
+++ main.c
@@ -2,16 +2,18 @@
 #define PI 3.14
 int main(void) {
     setbuf(stdout,0);
-    //realational operators
-    // > < >= <= != ==
-    int a = 10;
+    //logical operators
+    //and &&
+    // return 1 if both statements are true
+    // or ||
+    // logical not !
+
+    int a = 100;
     int b = 15;
-    printf("%d\n",a>b);
-    printf("%d\n",a<b);
-    printf("%d\n",a>=b);
-    printf("%d\n",a<=b);
-    printf("%d\n",a==b);
-    printf("%d\n",a!=b);
+    int c = 600;
+    printf("is a big %d\n",a>b&&a>c);
+    printf("%d\n",a>b||a>c);
+    printf("%d\n",!0);
 
 
 return 0;
