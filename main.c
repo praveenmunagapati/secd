98886dafae1d8fc9343de5248e86df100d70a44c
praveenmun <mpraveenpro@gmail.com>
2026-09-11 12:00:15 +0530

    Add selection statements and comparisons in main.c

--- main.c
+++ main.c
@@ -2,7 +2,21 @@
 int main(void)
 {
     setbuf(stdout,0);
-    
+    //slection statements
+    //if else switch case default break
+    int a = 10;
+    int b = 15;
+    int result = a>b;
+    if (result){
+        printf("a is big");
+    }
+    result = b>a;
+    if (result) {
+        printf("b is big");
+    }
 
+    if (-7) {
+        printf("praneetha");
+    }
     return 0;
 }
