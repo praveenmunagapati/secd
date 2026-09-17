1325da81690ff8c2a9c1185758b4baef1aef93e6
praveenmun <mpraveenpro@gmail.com>
2026-09-17 13:53:54 +0530

    Refactor main.c to remove unused functions and implement max/min calculation for three numbers

--- main.c
+++ main.c
@@ -1,41 +1,18 @@
 #include<stdio.h>
-
-
-/*
- *      returntype functionaname(pramaters only declarations){
- *          statemetns
- *          return returntype;
- *      }
- **/
-// 1 without parameter without return
-void wopwor(void) {
-    printf("type 1\n");
-    return;
-}
-// 2 with parameter without return
-void wpwor(int a) {
-    printf("%d\n",a);
-    return;
-}
-// 3 without parameter with return
-int wopwr(void) {
-    return 5;
-}
-// with parameter with return
-int wpwr(int a,int b) {
-    return a+b;
-}
-
 int main(void) {
     setbuf(stdout, 0);
-
-    //FUNCTIONS
-    wopwor();
-    wpwor(15.5);
-    int returnvalue = wopwr();
-    printf("%d\n",returnvalue);
-    int sum = wpwr(8,9);
-    printf("%d\n",sum);
+    //a) Write a program for finding the max and min from the three numbers.
+    int a =15,b = 15,c=150,max = 0,min = 0;
+    if (a>=b && a>=c) {
+        max = a;
+    }
+    if (b>=a && b>=c) {
+        max = b;
+    }
+    if (c>=a && c>=b) {
+        max = c;
+    }
+    printf("%d\n",max);
 
     return 0;
 }
