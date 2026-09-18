9ec0b648302c0e780566a52bece4d91f8f25c866
praveenmun <mpraveenpro@gmail.com>
2026-09-18 10:51:52 +0530

    Refactor CMakeLists.txt to include mylib.h and mylib.c in the build process; clean up main.c by removing unused binary printing code

--- main.c
+++ main.c
@@ -1,37 +1,8 @@
 #include<stdio.h>
-
-void printbinary(unsigned int num) {
-    for (int i = 128;i!=0;i>>=1) {
-        printf("%d",num&i?1:0);
-    }
-    printf("\n");
-    return;
-}
+#include "myfunctions/mylib.h"
 int main(void) {
     setbuf(stdout, 0);
-    //d) Write a program that shows
-    //the binary equivalent of a given positive number between 0 to 255.
-
-    unsigned int num =  179;
-
-    //&
-    //10 -> 1010
-    // 8  -> 1000
-    //10 -> 1010
-    // 4   -> 0100
-    //10 -> 1010
-    // 2   -> 0010
-    //10 -> 1010
-    // 1  -> 0001
-    for (int i = 128;i!=0;i>>=1) {
-        printf("%d",num&i?1:0);
-    }
-    printf("\n");
-
     printbinary(188);
-    printbinary(59);
-    printbinary(16);
-    printbinary(1);
-    printbinary(15);
+    printf("%f",simpleinterest(10000,16,2.6));
     return 0;
 }
