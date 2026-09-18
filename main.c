fb3240166b8cf4353c848898b5dcf0bb733a651b
praveenmun <mpraveenpro@gmail.com>
2026-09-18 11:12:25 +0530

    Refactor main.c to remove unused function calls and enhance floating-point output precision

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include<stdio.h>
-#include "myfunctions/mylib.h"
+#include<math.h>
 int main(void) {
     setbuf(stdout, 0);
-    printbinary(188);
-    printf("%f",simpleinterest(10000,16,2.6));
+    // printf("%lf\n",floor(1.6));
+    // printf("%lf\n",ceil(1.6));
+    // printf("%lf\n",sin(0));
+    // printf("%lf\n",cos(0));
+    // printf("%lf\n",sin(90));
+    // printf("%lf\n",pow(2,8));
+    // printf("%lf\n",sqrt(2));
+
+
+    printf("%0.25lf\n",0.1);
+    printf("%0.25lf\n",0.2);
+    printf("%0.25lf\n",0.3);
+    printf("%0.25lf\n",0.1+0.2);
+
+    if((0.1+0.2)==0.3) {
+        printf("true");
+    }
+    double i= 0.1;
+    for ( ; i <= 100000000 ; i+=0.1);
+    printf("%0.25lf\n",i);
+
+
     return 0;
 }
