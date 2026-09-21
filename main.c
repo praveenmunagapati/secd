026073f84f285d51c46395b2c620fdd0865945dc
praveenmun <mpraveenpro@gmail.com>
2026-09-21 11:56:26 +0530

    Refactor main.c to remove commented-out code and streamline maximum value calculation in an array

--- main.c
+++ main.c
@@ -1,56 +1,20 @@
 #include<stdio.h>
 #include<math.h>
+int maxarrayofints(int arrayofints[ ]) {
+    int max = 0;
+    max = arrayofints[0] ;
 
+    for (int i = 0;i<10;i++) {
+        if (max<=arrayofints[i]) {
+            max = arrayofints[i];
+        }
+    }
+    arrayofints[9] = 500000;
+    return max;
+}
 int main(void) {
     setbuf(stdout, 0);
-    //biggest 10 number
-    /*int num1,num2,num3,num4,num5,num6,num7,num8,num9,num10,max;
-    num1 = 1;
-    num2 = 5;
-    num3 = 6;
-    num4 = 2;
-    num5 = 9;
-    num6 = 15;
-    num7 = 8;
-    num8 = 12;
-    num9 = 45;
-    num10 = 150;
-   if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num1;
-    }
-    if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num1;
-    }
-
-    if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num1;
-    }    if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num1;
-    }
-    if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num1;
-    }
-    if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num1;
-    }
-    if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num1;
-    }
-    if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num1;
-    }
-    if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num1;
-    }
-    if (num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2&&num1>=num2) {
-        max = num10;
-    }*/
-    // char arrayofchars[10];
-    // printf("%d\n",sizeof(arrayofchars));
-    // float arrayoffloats[10];
-    // printf("%d\n",sizeof(arrayoffloats));
     int arrayofints[10];
-    //printf("%d\n",sizeof(arrayofints));
     arrayofints[0] = 5;
     arrayofints[1] = 6;
     arrayofints[2] = 7;
@@ -61,30 +25,36 @@ int main(void) {
     arrayofints[7] = 89;
     arrayofints[8] = 50;
     arrayofints[9] = 50;
-    //a[10] = 50;
-    //
-    printf("%d\n",arrayofints[8]);
-    printf("%d\n",sizeof(arrayofints[0]));
+    // int x[ ] = {9,8,7};//compiler automatically calculates sizes allocates memeory
 
-    int var = 0;
-    while (var<10) {
-        printf("%d\n",arrayofints[var]);
-        var = var + 1;
+
+
+    int max = maxarrayofints(arrayofints);
+
+    printf("max is %d\n",max);
+    printf("max is %d\n",arrayofints[9]);
+    for (int i = 0;i<10;i++) {
+            printf("%d\n",arrayofints[i]);
     }
-    printf("%d\n",sizeof(arrayofints[-4]));//uncheked bounds
-    printf("%d\n",sizeof(arrayofints[40]));//uncheked bounds
-    int max = 0;
-    max = arrayofints[0] ;
+    printf("%p\n",arrayofints);
+    printf("%p\n",&arrayofints[0]);
+    for (int i = 0;i<10;i++) {
+        printf("%p\n",&arrayofints[i]);
+    }
+    int *pa = arrayofints;
+    printf("%d\n",*pa);
+    printf("%d\n",*(pa+1));
+    printf("%d\n",*(pa+2));
 
     for (int i = 0;i<10;i++) {
-        if (max<=arrayofints[i]) {
-            max = arrayofints[i];
-        }
+        printf("%d\n",*(pa+i));
     }
-    printf("max is %d\n",max);
+    for (int i = 0;i<10;i++) {
+        printf("%p\n",(pa+i));
+    }
 
-
-
-
+    for (int i = 0;i<10;i++) {
+        printf("%d\n",*(arrayofints+i));//arrays are better pointers
+    }
     return 0;
 }
