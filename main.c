4a69f203d16d249de8462a1f48e372671bef7312
praveenmun <mpraveenpro@gmail.com>
2026-09-23 11:39:06 +0530

    Refactor main.c to remove bubble sort algorithm comments and streamline sorting output

--- main.c
+++ main.c
@@ -16,32 +16,26 @@ int main(void) {
     setbuf(stdout, 0);
     //searching in linear array
     int array[10] = {15,78,14,59,3,45,72,9,6,7};
-    /*
-    *   The 5 Steps of Bubble Sort
-        Start: Begin at the very first element (index 0) of the list.
-        Compare: Look at two neighboring elements side by side.
-        Swap: If the first element is bigger than the second element, switch their places so the larger value moves to the right.
-        Repeat for the pass: Move to the next pair of neighbors and repeat the comparison and swapping until you reach the end of the unsorted part of the list.
-        Continue sorting: Go back to the start of the list for a new pass, ignoring the elements that are already sorted at the end, and stop when a full pass happens with zero swaps.
-     */
-    printf("before sorting\n");
+
+    printf("\nbefore sorting\n");
     for (int i = 0; i < 10; ++i) {
         printf("%d\t",array[i]);
     }
-    printf("\n");
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 10; ++j) {
-            if (array[i]<array[j]) {
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-        }
-    }
-    printf("after sorting\n");
+    bubblesort(array);
+    printf("\nafter sorting\n");
     for (int i = 0; i < 10; ++i) {
         printf("%d\t",array[i]);
     }
 
+    int array1[10] = {15,78,14,8,3,74,72,9,6,7};
+    printf("\nbefore sorting\n");
+    for (int i = 0; i < 10; ++i) {
+        printf("%d\t",array1[i]);
+    }
+    bubblesort(array1);
+    printf("\nafter sorting\n");
+    for (int i = 0; i < 10; ++i) {
+        printf("%d\t",array1[i]);
+    }
     return 0;
 }
