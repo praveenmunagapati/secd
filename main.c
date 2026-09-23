a4a80d62ee0bccf94070bb71d414bcf0cae7eb1a
praveenmun <mpraveenpro@gmail.com>
2026-09-23 11:15:19 +0530

    Refactor main.c to remove unused isinarray function and related key search logic

--- main.c
+++ main.c
@@ -1,42 +1,26 @@
 #include<stdio.h>
 #include<math.h>
-int isinarray(int *parray,int key) {
-    int flag = 0;
-    for (int i = 0;i<10;i++) {
-        if (key == parray[i]) {
-            flag = 1;
-            break;
-        }
-    }
-    return flag;
-}
+
 int main(void) {
     setbuf(stdout, 0);
     //searching in linear array
     int array[10] = {15,78,14,59,3,45,72,9,6,7};
-    int key = 45;
-    int flag = 0;
-    // for (int i = 0;i<10;i++) {
-    //     if (key == array[i]) {
-    //         flag = 1;
-    //         break;
-    //     }
-    // }
-
-    flag = isinarray(array,key);
-    if (flag) {
-        printf("key found\n");
-    }else {
-        printf("key not found\n");
-    }
-    key = 89;
-    flag = isinarray(array,key);
-    if (flag) {
-        printf("key found\n");
-    }else {
-        printf("key not found\n");
-    }
-    
-
+    //swaping
+    int a = 15;
+    int b = 20;
+    printf("before\n");
+    printf("a = %d\n",a);
+    printf("b = %d\n",b);
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("after\n");
+    printf("a = %d\n",a);
+    printf("b = %d\n",b);
+    printf("without third variable\n");
+    a = a*b/(b=a);
+    printf("a = %d\n",a);
+    printf("b = %d\n",b);
     return 0;
 }
