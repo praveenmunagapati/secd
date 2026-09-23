ef74f58897fe629c7f3692d74e00b1c9e76a8a61
praveenmun <mpraveenpro@gmail.com>
2026-09-23 11:03:30 +0530

    Refactor main.c to update key value for array search and enhance output clarity

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(void) {
     setbuf(stdout, 0);
     //searching in linear array
     int array[10] = {15,78,14,59,3,45,72,9,6,7};
-    int key = 500;
+    int key = 45;
     int flag = 0;
     // for (int i = 0;i<10;i++) {
     //     if (key == array[i]) {
@@ -29,5 +29,14 @@ int main(void) {
     }else {
         printf("key not found\n");
     }
+    key = 89;
+    flag = isinarray(array,key);
+    if (flag) {
+        printf("key found\n");
+    }else {
+        printf("key not found\n");
+    }
+    
+
     return 0;
 }
