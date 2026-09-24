5cc870adf5fba379401aa05f46370909030f5e05
praveenmun <mpraveenpro@gmail.com>
2026-09-24 14:55:41 +0530

    Refactor main.c to remove unused 2D arrays and streamline pointer usage for array printing

--- main.c
+++ main.c
@@ -2,29 +2,40 @@
 int main(void) {
     setbuf(stdout, 0);
     int array2d[2][2] = {{1,2},{3,4}};
-    int barray2d[2][2] = {{1,2},{3,4}};
-    int rarray2d[2][2] ;
-    array2d[0][0] = 5;
-    array2d[0][1] = 6;
-    array2d[1][0] = 7;
-    array2d[1][1] = 9;
-    printf("array\n");
+    int *parray2d[2];
+    parray2d[0] =  array2d[0];
+    parray2d[1] =  array2d[1];
+    printf("%p\n", array2d[0]);
+    printf("%p\n", array2d[1]);
+    printf("%p\n",parray2d[0]);
+    printf("%p\n",parray2d[1]);
+    printf("%d\n",parray2d[0][0]);
+    printf("%d\n",parray2d[0][1]);
+    printf("%d\n",parray2d[1][0]);
+    printf("%d\n",parray2d[1][1]);
+    int (*pforarray2d)[2] = array2d;
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 2; ++j) {
-            printf("%d\t",array2d[i][j]);
+            printf("%d\n",pforarray2d[i][j]);
         }
-        printf("\n");
     }
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 2; ++j) {
-            rarray2d[i][j] = array2d[i][j] + barray2d[i][j];
+            printf("%d\n",*(*(pforarray2d + i) + j));
         }
     }
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            printf("%d\t",rarray2d[i][j]);
-        }
-        printf("\n");
+    int array[5] = {4,5,6,1,2};
+    for (int i = 0; i < 5; ++i) {
+        printf("%d \t",array[i]);
     }
+    for (int i = 0; i < 5; ++i) {
+        printf("%d \t",i[array]);
+    }
+    for (int i = 0; i < 5; ++i) {
+        printf("%d \t",*(array +i));
+    }
+    printf("%d \t",array[4]);
+    printf("%d \t",4[array]);
+
     return 0;
 }
