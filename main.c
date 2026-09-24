879280f8a380c4e1d86210255f6faa3ec72b7c09
praveenmun <mpraveenpro@gmail.com>
2026-09-24 13:51:05 +0530

    Refactor main.c to remove unused enums and streamline array operations

--- main.c
+++ main.c
@@ -1,45 +1,30 @@
 #include<stdio.h>
-#include<string.h>
-enum WEEK {
-    MONDAY=-5,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
-};
-enum MACHINE {
-    TEA=1,
-    COFFEE,
-    BOOST,
-
-};
 int main(void) {
     setbuf(stdout, 0);
-    printf("%d\n",sizeof(enum WEEK));
-    printf("%d\n",MONDAY);
-    printf("%d\n",TUESDAY);
-    printf("%d\n",WEDNESDAY);
-    printf("%d\n",THURSDAY);
-    printf("%d\n",FRIDAY);
-    printf("%d\n",SATURDAY);
-    printf("%d\n",SUNDAY);
-   //MONDAY = 1;//error
-
-    int choice = 4;
-    switch (choice) {
-        case 1 : printf("you get tea \n");break;
-        case 2 : printf("you get coffee \n");break;
-        case 3 : printf("you get boost \n");break;
-            default:printf("wrong choice\n");break;
+    int array2d[2][2] = {{1,2},{3,4}};
+    int barray2d[2][2] = {{1,2},{3,4}};
+    int rarray2d[2][2] ;
+    array2d[0][0] = 5;
+    array2d[0][1] = 6;
+    array2d[1][0] = 7;
+    array2d[1][1] = 9;
+    printf("array\n");
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            printf("%d\t",array2d[i][j]);
+        }
+        printf("\n");
     }
-    choice =COFFEE;
-    switch (choice) {
-        case TEA : printf("you get tea \n");break;
-        case COFFEE : printf("you get coffee \n");break;
-        case BOOST : printf("you get boost \n");break;
-        default:printf("wrong choice\n");break;
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            rarray2d[i][j] = array2d[i][j] + barray2d[i][j];
+        }
+    }
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            printf("%d\t",rarray2d[i][j]);
+        }
+        printf("\n");
     }
     return 0;
 }
