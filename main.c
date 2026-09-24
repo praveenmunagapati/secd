24272dc7a54afc912c0c2e34aeb857dcf831923e
praveenmun <mpraveenpro@gmail.com>
2026-09-24 13:32:23 +0530

    Refactor main.c to remove unused parallel arrays and streamline enum output

--- main.c
+++ main.c
@@ -1,33 +1,45 @@
 #include<stdio.h>
 #include<string.h>
+enum WEEK {
+    MONDAY=-5,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+};
+enum MACHINE {
+    TEA=1,
+    COFFEE,
+    BOOST,
+
+};
 int main(void) {
     setbuf(stdout, 0);
-    //parallel arrays
-    char *names[3];
-    int ppsmarks[3];
-    names[0]="praneetha";
-    names[1]="hanish";
-    names[2]="ram";
-    puts(names[0]);
-    puts(names[1]);
-    puts(names[2]);
+    printf("%d\n",sizeof(enum WEEK));
+    printf("%d\n",MONDAY);
+    printf("%d\n",TUESDAY);
+    printf("%d\n",WEDNESDAY);
+    printf("%d\n",THURSDAY);
+    printf("%d\n",FRIDAY);
+    printf("%d\n",SATURDAY);
+    printf("%d\n",SUNDAY);
+   //MONDAY = 1;//error
 
-    char anames[3][20] = {{"praneetha"},{"hanish"},{"ram"}};
-    printf("%d\n",sizeof(anames));
-    strcpy(anames[0],"hanish");
-    strcpy(anames[0],"praneetha");
-    strcpy(anames[0],"ram");
-    ppsmarks[0] = 80;
-    ppsmarks[1] = 90;
-    ppsmarks[2] = 78;
-    for (int i = 0; i < 3; ++i) {
-        printf("name %s marks %d\n",anames[i],ppsmarks[i]);
+    int choice = 4;
+    switch (choice) {
+        case 1 : printf("you get tea \n");break;
+        case 2 : printf("you get coffee \n");break;
+        case 3 : printf("you get boost \n");break;
+            default:printf("wrong choice\n");break;
     }
-    int id = 1;
-    for (int i = 0; i < 3; ++i) {
-        if (id==i) {
-            printf("name %s marks %d\n",anames[i],ppsmarks[i]);
-        }
+    choice =COFFEE;
+    switch (choice) {
+        case TEA : printf("you get tea \n");break;
+        case COFFEE : printf("you get coffee \n");break;
+        case BOOST : printf("you get boost \n");break;
+        default:printf("wrong choice\n");break;
     }
     return 0;
 }
